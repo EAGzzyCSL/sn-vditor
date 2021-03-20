5859c348d210c36e0c9d1c99b1703c44037c28b4
EAGzzyCSL <eagzzycsl@hotmail.com>
2021-03-20 12:32:04 +0800

    chore: config airbnb lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,58 @@
 module.exports = {
-  extends: ['react-app', 'react-app/jest', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  env: {
+    browser: true,
+  },
+  extends: [
+    'airbnb',
+    'plugin:eslint-plugin-import/errors',
+    'plugin:eslint-plugin-import/warnings',
+    'prettier',
+  ],
+  rules: {
+    'import/extensions': [
+      'error',
+      {
+        tsx: 'never',
+        ts: 'never',
+        jsx: 'never',
+        js: 'never',
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'sibling',
+          'parent',
+          'index',
+          'object',
+          'unknown',
+        ],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        'newlines-between': 'always',
+      },
+    ],
+    'import/prefer-default-export': 'off',
+  },
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:eslint-plugin-import/typescript',
+      ],
+      rules: {
+        'no-shadow': 'off',
+        'no-use-before-define': 'off',
+        'react/jsx-filename-extension': ['error', { extensions: ['.tsx'] }],
+      },
+    },
+  ],
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 import React from 'react'
 
-function App() {
+function App(): JSX.Element {
   return <div className='App'>sn-editor-template</div>
 }
 
