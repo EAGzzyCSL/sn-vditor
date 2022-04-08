8a24f3724a63762b0731d8b6d11457b77e4dea5b
EAGzzyCSL <eagzzycsl@hotmail.com>
2022-04-08 22:31:31 +0800

    feat: 不使用vditor的默认cdn

--- src/VditorEditor/options.ts
+++ src/VditorEditor/options.ts
@@ -1,4 +1,8 @@
-export default {
+const SELF_CDN = 'vditor-cdn-self'
+
+const createVditorOptions = (o: IOptions): IOptions => o
+
+export default createVditorOptions({
   icon: 'material',
   placeholder: 'type here to start new note',
   width: '100%',
@@ -13,6 +17,17 @@ export default {
     delay: 100,
     hljs: {
       lineNumber: true,
+    },
+    theme: {
+      // 照搬 vditor/src/ts/constants 下的定义
+      current: 'light',
+      list: {
+        'ant-design': 'Ant Design',
+        dark: 'Dark',
+        light: 'Light',
+        wechat: 'WeChat',
+      },
+      path: `${SELF_CDN}/dist/css/content-theme`,
     },
   },
   toolbar: [
@@ -43,4 +58,11 @@ export default {
     'content-theme',
     'preview',
   ] as string[],
-} as const
+  /**
+   * vditor 会动态加载 jsdelivr CDN 的内容
+   * 其实就是它 npm 包的 dist
+   * 这里直接把它软链接了，打包的时候再手动copy进去就好了
+   * https://www.npmjs.com/package/vditor
+   */
+  cdn: SELF_CDN,
+})
